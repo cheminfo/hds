9da01b796538a6d1d08b09b6c62959795abd391a
Michaël Zasso <mic.besace@gmail.com>
2014-09-29 14:13:14 +0200

    add method to batch insert / update data

--- src/entry.js
+++ src/entry.js
@@ -2,11 +2,15 @@
 
 var Kind = require('./kind'),
     util = require('./util'),
-    Promise = require('bluebird');
+    Promise = require('bluebird'),
+    extend = require('extend'),
+    mongoose = require('mongoose'),
+    ObjectID = mongoose.mongo.ObjectID,
+    async = require('async');
 
 exports.create = function createEntry(kind, value, options) {
 
-    if(arguments.length === 2) {
+    if (arguments.length === 2) {
         options = value;
         value = null;
     }
@@ -16,7 +20,7 @@ exports.create = function createEntry(kind, value, options) {
     var kindModel = Kind.getSync(kind);
     var entry = new kindModel(value);
     // TODO handle options
-    if(!options.owner) {
+    if (!options.owner) {
         throw new Error('cannot create an entry without owner');
     }
     entry._ow = options.owner;
@@ -24,16 +28,340 @@ exports.create = function createEntry(kind, value, options) {
 
 };
 
+// Insert, update and/or delete multiple entries with a single instruction
+/*
+Supported actions:
+- insert (default) : search entries with query. If no entry found create a new one, if one entry found modify its value, if more than one entry found error, then treat children
+- delete : remove all entries that match query then stop
+- add : insert new entry without searching, then treat children
+- replace : remove all entries that match query, then create entry with value, then treat children
+ */
+exports.batch = function (data, options, callback) {
+
+    var prom;
+    if(data instanceof Array) {
+        prom = Promise.all(data.map(function(dataVal) {
+            return exports.batch(dataVal, options);
+        }));
+    } else {
+        prom = new Promise(function (resolve, reject) {
+
+            if (!data.kind) {
+                return reject(new Error('Batch method requires a kind for each element'));
+            }
+
+            var action = data.action || 'insert';
+
+            options = options || {};
+            if (!options.owner) {
+                reject(new Error('Batch method requires an owner'));
+            } else {
+                if (options.parent) {
+                    Kind.get(options.parent.kind).then(function (parentModel) {
+                        parentModel.findById(options.parent.id, function (err, parent) {
+                            if (err) {
+                                reject(err);
+                            } else if (parent) {
+                                insertElement(parent);
+                            } else {
+                                reject(new Error('Parent ' + JSON.stringify(options.parent) + 'does not exist'));
+                            }
+                        });
+                    }, reject);
+                } else {
+                    insertElement();
+                }
+            }
+
+            function insertElement(parent) {
+                Kind.get(data.kind).then(function (KindModel) {
+
+                    var query = {
+                        $and: [
+                            extend({}, data.query, {
+                                _ow: options.owner
+                            })
+                        ]
+                    };
+
+                    if (parent) {
+                        var parentQuery = {
+                            kind: parent.getKind(),
+                            id: parent.id
+                        };
+                        query.$and.unshift({ _an: { $elemMatch: parentQuery } });
+                    } else {
+                        query.$and.unshift({ _an: { $size: 0 } });
+                    }
+
+                    var finalCallback = getFinalCallback(resolve, reject),
+                        nextCallback = getNextCallback(resolve, reject, data);
+
+                    function createNewEntry(value, callback) {
+                        var objData, obj;
+                        if(value) {
+                            objData = extend({}, value, { _ow: data.owner });
+                            obj = parent ? parent.createChild(data.kind, objData) : new KindModel(objData);
+                            obj.save(function (err, entry) {
+                                if (err) {
+                                    callback(err);
+                                } else {
+                                    if(value.attachments) {
+                                        async.each(value.attachments, addAttachment(entry), callback);
+                                    } else {
+                                        callback();
+                                    }
+                                }
+                            });
+                        } else {
+                            objData = extend({}, data.query, data.value, { _ow: data.owner });
+                            obj = parent ? parent.createChild(data.kind, objData) : new KindModel(objData);
+                            obj.save(function (err, entry) {
+                                if (err) {
+                                    nextCallback(err);
+                                } else {
+                                    if (data.attachments) {
+                                        async.each(data.attachments, addAttachment(entry), function (err) {
+                                            if(err) {
+                                                nextCallback(err);
+                                            } else {
+                                                nextCallback(null, entry);
+                                            }
+                                        });
+                                    } else {
+                                        nextCallback(entry);
+                                    }
+                                }
+                            });
+                        }
+                    }
+
+                    function addNewEntries(err) {
+                        if (err) {
+                            return reject(err);
+                        }
+                        var values = data.values || [ { value: data.value, attachments: data.attachments } ];
+                        async.each(values, createNewEntry, finalCallback);
+                    }
+
+                    if (action === 'add') {
+                        createNewEntry();
+                    } else {
+                        KindModel.find(query, function (err, res) {
+                            if (err) {
+                                reject(err);
+                            } else if (res.length) {
+                                if (action === 'delete') {
+                                    async.each(res, removeEntry, finalCallback);
+                                } else if (action === 'replace') {
+                                    async.each(res, removeEntry, addNewEntries);
+                                } else if (action === 'insert') {
+                                    if(res.length > 1) {
+                                        reject(new Error('Query lacks specificity. '+res.length+' matching documents found'));
+                                    } else {
+                                        res = res[0];
+                                        for (var i in data.value) {
+                                            if (data.value.hasOwnProperty(i)) {
+                                                res[i] = data.value[i];
+                                            }
+                                        }
+                                        res.save(nextCallback);
+                                    }
+                                } else {
+                                    reject(new Error('Unknown action: ' + action));
+                                }
+                            } else {
+                                if (action === 'delete') {
+                                    reject(new Error('Entry not found, cannot remove'));
+                                } else if (action === 'replace') {
+                                    addNewEntries();
+                                } else if (action === 'insert') {
+                                    createNewEntry();
+                                } else {
+                                    reject(new Error('Unknown action: ' + action));
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+
+        });
+    }
+    return util.bindPromise(prom, callback);
+
+};
+
+function addAttachment(entry) {
+    return function addAttachmentToEntry(attachment, callback) {
+        entry.createAttachment({
+            data: new Buffer(attachment.value, attachment.encoding || 'utf8'),
+            name: attachment.filename,
+            contentType: attachment.contentType
+        }, callback);
+    };
+}
+
+function removeEntry(entry, callback) {
+    entry.remove(callback);
+}
+
+function getFinalCallback(resolve, reject) {
+    return function finalCallback(err) {
+        if (err) {
+            return reject(err);
+        }
+        resolve();
+    }
+}
+
+function getNextCallback(resolve, reject, data) {
+    return function nextCallback(err, entry) {
+        if (err) {
+            return reject(err);
+        }
+        // TODO attachments
+        if(data.children) {
+            var promises = data.children.map(function(child) {
+                return _batch(child, entry);
+            });
+            Promise.all(promises).then(resolve, reject);
+        } else {
+            resolve();
+        }
+    }
+}
+
+function _batch(data, parent) {
+    return new Promise(function (resolve, reject) {
+
+        if (!data.kind) {
+            return reject(new Error('Batch method requires a kind for each element'));
+        }
+
+        var action = data.action || 'insert';
+
+        var parentQuery = {
+            kind: parent.getKind(),
+            id: ObjectID(parent.id)
+        };
+
+        var ancestors = parent._an.slice();
+        ancestors.push(parentQuery);
+
+        Kind.get(data.kind).then(function (KindModel) {
+
+            var query = {
+                $and: [
+                    { _an: { $elemMatch: parentQuery } },
+                    { _ow: parent.owner, _an: ancestors }
+                ]
+            };
+            if(data.query) {
+                query.$and.push(extend({}, data.query))
+            }
+
+            var finalCallback = getFinalCallback(resolve, reject),
+                nextCallback = getNextCallback(resolve, reject, data);
+
+            function createNewEntry(value, callback) {
+                var obj;
+                if(value) {
+                    obj = parent.createChild(data.kind, extend({}, value));
+                    obj.save(function (err, entry) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            if(value.attachments) {
+                                async.each(value.attachments, addAttachment(entry), callback);
+                            } else {
+                                callback();
+                            }
+                        }
+                    });
+                } else {
+                    obj = parent.createChild(data.kind, extend({}, data.query, data.value));
+                    obj.save(function (err, entry) {
+                        if (err) {
+                            nextCallback(err);
+                        } else {
+                            if (data.attachments) {
+                                async.each(data.attachments, addAttachment(entry), function (err) {
+                                    if(err) {
+                                        nextCallback(err);
+                                    } else {
+                                        nextCallback(null, entry);
+                                    }
+                                });
+                            } else {
+                                nextCallback(entry);
+                            }
+                        }
+                    });
+                }
+            }
+
+            function addNewEntries(err) {
+                if (err) {
+                    return reject(err);
+                }
+                var values = data.values || [ { value: data.value, attachments: data.attachments } ];
+                async.each(values, createNewEntry, finalCallback);
+            }
+
+            if (action === 'add') {
+                createNewEntry();
+            } else {
+                KindModel.find(query, function (err, res) {
+                    if (err) {
+                        reject(err);
+                    } else if (res.length) {
+                        if (action === 'delete') {
+                            async.each(res, removeEntry, finalCallback);
+                        } else if (action === 'replace') {
+                            async.each(res, removeEntry, addNewEntries);
+                        } else if (action === 'insert') {
+                            if (res.length > 1) {
+                                reject(new Error('Query lacks specificity. '+res.length+' matching documents found'));
+                            } else {
+                                res = res[0];
+                                for (var i in data.value) {
+                                    if (data.value.hasOwnProperty(i)) {
+                                        res[i] = data.value[i];
+                                    }
+                                }
+                                res.save(nextCallback);
+                            }
+                        } else {
+                            reject(new Error('Unknown action: ' + action));
+                        }
+                    } else {
+                        if (action === 'delete') {
+                            reject(new Error('Entry not found, cannot remove'));
+                        } else if (action === 'replace') {
+                            addNewEntries();
+                        } else if (action === 'insert') {
+                            createNewEntry();
+                        } else {
+                            reject(new Error('Unknown action: ' + action));
+                        }
+                    }
+                });
+            }
+        });
+    });
+}
+
 exports.insertTree = function (tree, options, callback) {
 
     options = options || {};
-    if(!options.owner) {
+    if (!options.owner) {
         throw new Error('cannot create an entry without owner');
     }
 
     var prom = new Promise(function (resolve, reject) {
 
-        if(!tree) {
+        if (!tree) {
             return reject(new Error('missing tree parameter'));
         }
 
@@ -43,11 +371,11 @@ exports.insertTree = function (tree, options, callback) {
             rootVal._ow = options.owner;
             rootVal.save(function (err, rootVal) {
 
-                if(err) {
+                if (err) {
                     return reject(err);
                 }
 
-                if(!tree.children) {
+                if (!tree.children) {
                     return resolve(rootVal);
                 }
 
@@ -76,12 +404,12 @@ function appendChildren(parent, children) {
 
     return new Promise(function (resolve, reject) {
 
-        if(!children instanceof Array) {
+        if (!children instanceof Array) {
             return reject(new Error('children element must be an array'));
         }
 
         var childrenProm = [];
-        for(var i = 0; i < children.length; i++) {
+        for (var i = 0; i < children.length; i++) {
             childrenProm.push(appendChild(parent, children[i]));
         }
 
@@ -98,10 +426,10 @@ function appendChild(parent, child) {
         Kind.get(child.kind).then(function () {
 
             parent.createChild(child.kind, child.value).save(function (err, childObj) {
-                if(err) {
+                if (err) {
                     return reject(err);
                 }
-                if(!child.children) {
+                if (!child.children) {
                     resolve();
                 } else {
                     appendChildren(childObj, child.children).then(resolve, reject);
